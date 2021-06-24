85f8e1fcedf59a5bf3f0a382bafbac020c0cd40d
Jérémie Fouché <jeremie.fouche@gmail.com>
2021-06-24 16:58:24 +0200

    better "module" managements

--- src/app.ts
+++ src/app.ts
@@ -21,9 +21,23 @@ class Database {
   }
 
   async add_module(name: string) {
-    const module = { name };
-    const result = await this.db.collection('modules').insertOne(module);
-    console.log(result);
+    const doc = { name };
+    return this.db.collection('modules').insertOne(doc).then((result) => {
+      return result.ops[0];
+    });
+  }
+
+  async get_requirements(id: string) {
+    const filter = {
+      module: `${id}`
+    };
+    return this.db.collection('requirements').find(filter).toArray()
+  }
+
+  async add_requirement(id_module: string, text: string) {
+    const doc = {module: id_module, text };
+    const result = await this.db.collection('requirements').insertOne(doc);
+    console.log(result.ops[0]);
   }
 }
 
@@ -39,24 +53,35 @@ app.use(express.json());
 
 const port = 3000;
 
-app.get('/', async (req, res) => {
+// GET /modules
+app.get('/modules', async (req, res) => {
   const modules = await database.get_modules();
   console.log(modules);
-  res.send('The sedulous hyena ate the antelope!');
-  // db.collection('modules').find().toArray().then(m => {
-  //   console.log(m);
-  // });
+  res.send(JSON.stringify(modules));
 });
 
+// POST /modules {"name": "..."}
 app.post('/modules', async (req, res) => {
   const name = req.body.name;
   if (!name) return res.status(400).send({
     message: "POST /modules : missing name"
   });
-  database.add_module(name);
+  let m = await database.add_module(name);
+  console.log(m);
   res.send('New post added');
 });
 
+// GET /module/<ID>/requirements
+app.get('/module/:id/requirements', async (req, res) => {
+  const id = req.params.id;
+  const reqs = await database.get_requirements(id);
+  console.log(reqs);
+  console.log(JSON.stringify(reqs));
+  res.send(JSON.stringify(reqs));
+});
+
+
+// ------------------------------------
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
 });
--- test/curl.cmd
+++ test/curl.cmd
@@ -1,4 +1,7 @@
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:3000/
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:3000/modules
 
 curl -d "{}" -H "Content-Type: application/json" -X POST http://localhost:3000/modules
 curl -d "{ \"name\" : \"SSDD\" }" -H "Content-Type: application/json" -X POST http://localhost:3000/modules
+
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:3000/module:<ID>/requirements
+
